eec80a36c938f5fbcbc22fbbdfb875a1d7697da8
viig99 <accio.arjun@gmail.com>
2025-06-26 17:22:55 -0400

    test config setters

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,3 +10,21 @@ def test_default_configs():
     assert config.projection_dimension == 0
     assert config.fill_empty_partitions is False
     assert config.final_projection_dimension == 0
+
+def test_config_setters():
+    config = m.FixedDimensionalEncodingConfig()
+    config.set_dimension(10)
+    config.set_num_repetitions(2)
+    config.set_num_simhash_projections(5)
+    config.set_seed(42)
+    config.set_projection_dimension(3)
+    config.enable_fill_empty(True)
+    config.set_final_projection_dimension(4)
+
+    assert config.dimension == 10
+    assert config.num_repetitions == 2
+    assert config.num_simhash_projections == 5
+    assert config.seed == 42
+    assert config.projection_dimension == 3
+    assert config.fill_empty_partitions is True
+    assert config.final_projection_dimension == 4
