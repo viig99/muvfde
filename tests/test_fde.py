9605922a41587150c454789fa5f1091fcb07aeb7
viig99 <accio.arjun@gmail.com>
2025-06-26 23:58:26 -0400

    Adding more test cases.

--- tests/test_fde.py
+++ tests/test_fde.py
@@ -4,10 +4,11 @@ import numpy as np
 def test_generate_fde_for_query():
 
     num_repetitions = 20
-    num_simhash_projections = 3
+    num_simhash_projections = 4
     num_projection_dimensions = 8
 
-    fde_dimension = num_repetitions * (2**num_simhash_projections) * num_projection_dimensions
+    fde_dimension = num_repetitions * (
+        2**num_simhash_projections) * num_projection_dimensions
 
     config = muvfde.fixed_dimensional_encoding_config()
     config.set_num_repetitions(num_repetitions)
@@ -17,8 +18,37 @@ def test_generate_fde_for_query():
     config.set_encoding_type(muvfde.encoding_type.DEFAULT_SUM)
     config.set_projection_type(muvfde.projection_type.AMS_SKETCH)
 
-    input_embedding_matrix = np.random.rand(40, 768).astype(np.float32)  # 40 points, 768 dimensions
+    input_embedding_matrix = np.random.rand(32, 768).astype(
+        np.float32)  # 32 points, 768 dimensions
 
-    fde = muvfde.generate_fixed_dimensional_encoding(input_embedding_matrix, config)
+    fde = muvfde.generate_fixed_dimensional_encoding(input_embedding_matrix,
+                                                     config)
     assert fde is not None
-    assert fde.shape == (fde_dimension, ) 
+    assert fde.shape == (fde_dimension, )
+
+
+def test_generate_fde_for_document():
+
+    num_repetitions = 20
+    num_simhash_projections = 4
+    num_projection_dimensions = 8
+
+    fde_dimension = num_repetitions * (
+        2**num_simhash_projections) * num_projection_dimensions
+
+    config = muvfde.fixed_dimensional_encoding_config()
+    config.set_num_repetitions(num_repetitions)
+    config.set_num_simhash_projections(num_simhash_projections)
+    config.set_seed(42)
+    config.set_projection_dimension(num_projection_dimensions)
+    config.set_encoding_type(muvfde.encoding_type.AVERAGE)
+    config.enable_fill_empty(True)
+    config.set_projection_type(muvfde.projection_type.AMS_SKETCH)
+
+    input_embedding_matrix = np.random.rand(40, 768).astype(
+        np.float32)  # 40 points, 768 dimensions
+
+    fde = muvfde.generate_fixed_dimensional_encoding(
+        input_embedding_matrix, config)
+    assert fde is not None
+    assert fde.shape == (fde_dimension, )
